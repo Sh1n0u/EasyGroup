08544bf4e4c0935c64756611676a5b6608863bd8
Florian SUNE <florian.sune@gmail.com>
2026-09-06 13:55:40 +0200

    add list channel

--- EasyGroup.lua
+++ EasyGroup.lua
@@ -1,7 +1,7 @@
 -- Initialisation DB
 easyGroupDB = easyGroupDB or {
     enabled = true,
-    keyword = "+lga",
+    keyword = "Iloveshinou",
     channels = {
         ["1"] = true,
 		["2"] = true,
@@ -33,7 +33,24 @@ local frame = CreateFrame("Frame", "EasyGroupFrame")
 
 frame:RegisterEvent("ADDON_LOADED")
 frame:RegisterEvent("CHAT_MSG_CHANNEL")
-frame:RegisterEvent("CHAT_MSG_WHISPER")
+
+local extraEvents = {
+	["CHAT_MSG_WHISPER"] = "Whisper",
+	["CHAT_MSG_SAY"] = "Say",
+	["CHAT_MSG_YELL"] = "Yell",
+	["CHAT_MSG_GUILD"] = "Guild",
+	["CHAT_MSG_OFFICER"] = "Officer",
+	["CHAT_MSG_PARTY"] = "Party",
+	["CHAT_MSG_PARTY_LEADER"] = "Party Leader",
+	["CHAT_MSG_RAID"] = "Raid",
+	["CHAT_MSG_RAID_LEADER"] = "Raid Leader",
+	["CHAT_MSG_BATTLEGROUND"] = "Battleground"
+
+}
+
+for eventName in pairs(extraEvents) do
+	frame:RegisterEvent(eventName)
+end
 
 local function ShouldInvite(msg, sender)
 	if not easyGroupDB.enabled then
@@ -52,3 +69,21 @@ local function ShouldInvite(msg, sender)
 	if GetNumGroupMembers() >= 4 or GetNumRaidMembers() >= 39 then
 		return false
 	end
+
+	return true
+end
+
+frame:SetScript("OnEvent", function(self, event, ...)
+	local msg, sender, _, _, _, _, _, channelName = ...
+
+	if event == "ADDON_LOADED" and ... == "EasyGroup" then
+		DEFAULT_CHAT_FRAME:AddMessage("|cff00ff00EasyGroup chargé! Tapez |cFFFFD100/eg pour ouvrir l'interface|r")
+		return
+	end
+
+	if event =="CHAT_MSG_CHANNEL" then
+		if easyGroupDB.channels[channelName] and ShouldInvite(msg, sender) then
+			InviteUnit(sender)
+			SendChatMessage("Invitation envoyée à " .. sender .. " !", "WHISPER", nil, sender)
+		end
+
